69c2bbdd32f43fb5a837193ae0fc1e997838b6a9
Zijia Zhai <zijiazhai@gmail.com>
2018-11-01 15:07:08 -0500

    procedure-oriented to object-oriented

--- Main.py
+++ Main.py
@@ -3,6 +3,37 @@ import time
 from pygame.locals import *
 
 
+class HeroPlane(object):
+    def __init__(self, screen_temp):
+        self.x = 210
+        self.y = 700
+        self.screen = screen_temp
+        self.image = pygame.image.load("feiji/hero1.png")
+
+    def display(self):
+        self.screen.blit(self.image, (self.x, self.y))
+
+    def move_left(self):
+        self.x -= 5
+
+    def move_right(self):
+        self.x += 5
+
+
+def key_control(hero_temp):
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            print("exit")
+            exit()
+        elif event.type == KEYDOWN:
+            if event.key == K_a or event.key == K_LEFT:
+                hero_temp.move_left()
+            elif event.key == K_d or event.key == K_RIGHT:
+                hero_temp.move_right()
+            elif event.key == K_SPACE:
+                print('space')
+
+
 def main():
 
     # 1. create screen
@@ -12,31 +43,14 @@ def main():
     background = pygame.image.load("feiji/background.png")
 
     # 3. create airplane image
-    hero = pygame.image.load("feiji/hero1.png")
-
-    x = 210
-    y = 700
+    hero = HeroPlane(screen)
 
     while True:
         pygame.event.get()  #returns a list of all events currently in the event queue. always not responding if not
         screen.blit(background, (0, 0))
-        screen.blit(hero, (x, y))
+        hero.display()
         pygame.display.update()
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                print("exit")
-                exit()
-            elif event.type == KEYDOWN:
-                if event.key == K_a or event.key == K_LEFT:
-                    print('left')
-                    x -= 5
-                elif event.key == K_d or event.key == K_RIGHT:
-                    print('right')
-                    x += 5
-                elif event.key == K_SPACE:
-                    print('space')
-
+        key_control(hero)
         time.sleep(0.01)
 
 
